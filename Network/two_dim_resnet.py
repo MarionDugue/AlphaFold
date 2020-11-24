2ac41db979981d94dafdb7d52fd60d10d62cf573
MarionDugué <67784742+MarionDugue@users.noreply.github.com>
2020-11-24 21:01:44 +0000

    Comments

--- Network/two_dim_resnet.py
+++ Network/two_dim_resnet.py
@@ -1,9 +1,19 @@
+'''Creates a 2D residual network
+
+See 'DeepMind's algorithm to create a seperable layer' figure to get the big picture'''
+
 
 import torch.nn as nn
 from two_dim_convnet import make_conv_layer, make_conv_sep2d_layer
 from collections import OrderedDict
 
 class ResidualBlock(nn.Module):
+    """Creates a residual block
+    
+    AlphaFold is composed of 220 residual blocks (number of blocks with 
+    highest accuracy with least computational power) cycling through dilations 1,2,4,8
+    
+    """
     def __init__(self, in_channels,
                        out_channels,
                        layer_name,
@@ -19,19 +29,21 @@ class ResidualBlock(nn.Module):
         self.batch_norm = batch_norm
         self.dropout_keep_prob = dropout_keep_prob
         self.channel_multiplier = channel_multiplier
-
+        
+        
+        #Batchnorm 
         if batch_norm:
             self.bn = nn.BatchNorm2d(in_channels, momentum=0.001, eps=0.001)
         self.elu = nn.ELU()
 
-        # 1x1 with half size
+        # Project down: 1x1 with half size (64*64)
         self.conv_1x1h = make_conv_layer(in_channels=in_channels,
                                          out_channels=in_channels // divide_channels_by,
                                          filter_size=1,
                                          non_linearity=True,
                                          batch_norm=batch_norm)
 
-        # 3x3 with half size
+        # Dilate: 3x3 with half size
         if channel_multiplier == 0:
             self.conv_3x3h = make_conv_layer(in_channels=in_channels // divide_channels_by,
                                              out_channels=in_channels // divide_channels_by,
@@ -47,13 +59,13 @@ class ResidualBlock(nn.Module):
                                                       batch_norm=batch_norm,
                                                       atrou_rate=atrou_rate)
 
-        # 1x1 back to normal size without relu
+        # Project up: 1x1 back to normal size without relu
         self.conv_1x1 = make_conv_layer(in_channels=in_channels // divide_channels_by,
                                         out_channels=out_channels,
                                         filter_size=1,
                                         non_linearity=False,
                                         batch_norm=False)
-
+        #dropout
         if dropout_keep_prob < 1.0:
             self.dropout = nn.Dropout(1-dropout_keep_prob)
 
@@ -90,6 +102,29 @@ def make_two_dim_resnet(num_features,
                         divide_channels_by=2,
                         resize_features_with_1x1=False,
                         dropout_keep_prob=1.0):
+    '''
+      Create a 2d resnet thanks to make_conv_layer method and ResidualBlock class
+      
+      Parameters
+    ----------
+    num_features,
+    num_predictions=1,
+    num_channels=32,
+    num_layers=2,
+    filter_size=3,
+    final_non_linearity=False,
+    batch_norm=False,
+    atrou_rates=None,
+    channel_multiplier=0,
+    divide_channels_by=2,
+    resize_features_with_1x1=False,
+    dropout_keep_prob=1.0
+    
+    Returns
+    -------
+    Sequential of layers consitutuing the 2d resnet'''
+            
+    #Note: à trou rate is equivalent to dilution factor
     if atrou_rates is None: atrou_rates = [1]
 
     layers = []
@@ -105,6 +140,7 @@ def make_two_dim_resnet(num_features,
             out_channels = num_predictions
             non_linearity = final_non_linearity
 
+        #For first and penulitmate layer: Create convolutional layer
         if i_layer == 0 or i_layer == num_layers - 1:
             layer_name = f'conv{i_layer+1}'
             initial_filter_size = 1 if resize_features_with_1x1 else filter_size
@@ -113,6 +149,8 @@ def make_two_dim_resnet(num_features,
                                         filter_size=initial_filter_size,
                                         non_linearity=non_linearity,
                                         atrou_rate=curr_atrou_rate)
+            
+        #For in-between layers, create residual blocks
         else:
             layer_name = f'res{i_layer+1}'
             conv_layer = ResidualBlock(in_channels=in_channels,
